078bc1ae4d47c975c20ac20b06185ac9d47540a7
wz-ml <zhou.pe@westmountcharter.com>
2020-09-01 17:25:36 -0600

    Updates

---
+++ precache-manifest.2bf654918b884a9d54144bfff279ed5f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9489cdcdd8279b7c02b91817d0bc2c0b",
+    "url": "/SC2MMR-React/index.html"
+  },
+  {
+    "revision": "61e65ab33f08ff3d9b35",
+    "url": "/SC2MMR-React/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "70cca7dd3468ff344b2a",
+    "url": "/SC2MMR-React/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "61e65ab33f08ff3d9b35",
+    "url": "/SC2MMR-React/static/js/2.d973c4cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SC2MMR-React/static/js/2.d973c4cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70cca7dd3468ff344b2a",
+    "url": "/SC2MMR-React/static/js/main.a05d5392.chunk.js"
+  },
+  {
+    "revision": "485d5fd30aa12ba3099e",
+    "url": "/SC2MMR-React/static/js/runtime-main.b6bde6a6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SC2MMR-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
